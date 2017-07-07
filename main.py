a28112261cf2e56f8b6a8b2d4e0742f0f482ccb1
William Toscano <William Toscano>
2017-07-07 16:07:39 -0700

    email error message added

--- main.py
+++ main.py
@@ -30,20 +30,32 @@ def validate_form():
     user_error = ""
     pass_error = ""
     v_error = ""
+    e_error = ""
     error_happend = False
     if request.method == 'POST':
-
-        if checkString(name) == True:
+        
+        if len(name) == 0:
+            user_error = "Please enter a username"
+            error_happend = True
+        elif checkString(name) == True:
             user_error = "Please enter good username"
             error_happend = True
-        if checkString(password) == True:
+        if len(password) == 0:
+            pass_error = "Please enter a password"
+            error_happend = True
+        elif checkString(password) == True:
             pass_error = "Please enter good password"
             error_happend = True
         if password != verify:
             v_error = "Password does not match"
             error_happend = True
+        if len(email) == 0:
+            e_error = ""
+        elif checkString(email) == True or "." not in email or "@" not in email:
+            e_error = "Please enter a good email"
+            error_happend = True   
         if error_happend == True:
-            return render_template(template, user_error=user_error, pass_error=pass_error, v_error=v_error)
+            return render_template(template, user_error=user_error, pass_error=pass_error, v_error=v_error, e_error=e_error)
 
         username = request.form['username']
         template = jinja_env.get_template('welcome.html')
